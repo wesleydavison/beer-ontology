71ff066270cf46f3921b0a913f3da91627fe7a86
Wesley Davison <davison.melo@gmail.com>
2014-07-24 08:53:29 -0300

    Write ontology in "ontology.owl" file

--- gen.py
+++ gen.py
@@ -2,23 +2,30 @@
 
 import xmltodict
 
+#opening input and output files
 with open ("ontology.xml", "r") as myfile:
     data=myfile.read()
+outFile=open('./ontology.owl', 'w+')
+
 
 #parsing the XML file
 xml = xmltodict.parse(data)
 root = 'beer'
+output = ""
 
 #generating the classes
 for beerClass in xml[root]:
-	print "<owl:Class rdf:ID=\"" + beerClass + "\">"
-	print "\t<rdfs:label xml:lang=\"en\">" + beerClass + "</rdfs:label>" 
+	output += "<owl:Class rdf:ID=\"" + beerClass + "\">" + '\n'
+	output += "\t<rdfs:label xml:lang=\"en\">" + beerClass + "</rdfs:label>" + '\n'
 	
 	#generating the subclasses
 	for subClass1 in xml[root][str(beerClass)]:
-		print "\t<owl:Class rdf:ID=\"" + subClass1 + "\">"
-		print "\t\t<rdfs:subClassOf rdf:resource=\"#" + beerClass + "\" />"
+		output += "\t<owl:Class rdf:ID=\"" + subClass1 + "\">" + '\n'
+		output += "\t\t<rdfs:subClassOf rdf:resource=\"#" + beerClass + "\" />" + '\n'
+		output += "\t</owl:Class>" + '\n'
 	
 	#closing class
-	print "</owl:Class>"
+	output += "</owl:Class>" + '\n'
 
+#writing in file
+outFile.write(output)
