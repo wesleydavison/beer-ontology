bff5808fb51c77e2a7c32af0bf20b60026af1846
Wesley Davison <davison.melo@gmail.com>
2014-07-24 09:27:22 -0300

    Add second level of classes

--- gen.py
+++ gen.py
@@ -16,15 +16,23 @@ output = ""
 #generating the classes
 for beerClass in xml[root]:
 
-	output += "<owl:Class rdf:ID=\"" + beerClass + "\">" + '\n'
-	output += "\t<rdfs:label xml:lang=\"en\">" + beerClass + "</rdfs:label>" + '\n'
+	output += "<owl:Class rdf:ID=\"" + beerClass.encode('utf-8') + "\">" + '\n'
+	output += "\t<rdfs:label xml:lang=\"en\">" + beerClass.encode('utf-8') + "</rdfs:label>" + '\n'
 	
 	#generating the first level of subclass
 	for subClass1 in xml[root][str(beerClass)]:
-		output += "\t<owl:Class rdf:ID=\"" + subClass1 + "\">" + '\n'
-		output += "\t\t<rdfs:label xml:lang=\"en\">" + subClass1 + "</rdfs:label>" + '\n'
-		output += "\t\t<rdfs:subClassOf rdf:resource=\"#" + beerClass + "\" />" + '\n'
+		output += "\t<owl:Class rdf:ID=\"" + subClass1.encode('utf-8') + "\">" + '\n'
+		output += "\t\t<rdfs:label xml:lang=\"en\">" + subClass1.encode('utf-8') + "</rdfs:label>" + '\n'
+		output += "\t\t<rdfs:subClassOf rdf:resource=\"#" + beerClass.encode('utf-8') + "\" />" + '\n'
 		output += "\t</owl:Class>" + '\n'
+
+		#generating second level of subclass
+		for subClass2 in xml[root][str(beerClass)][subClass1]:
+			output += "\t<owl:Class rdf:ID=\"" + subClass2.encode('utf-8') + "\">" + '\n'
+			output += "\t\t<rdfs:label xml:lang=\"en\">" + subClass2.encode('utf-8') + "</rdfs:label>" + '\n'
+			output += "\t\t<rdfs:subClassOf rdf:resource=\"#" + subClass1.encode('utf-8') + "\" />" + '\n'
+			output += "\t</owl:Class>" + '\n'
+
 	
 	#closing class
 	output += "</owl:Class>" + '\n'
