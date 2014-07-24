e59d2fdc33540de73b2843cf2fed33eb9ad3f924
Wesley Davison <davison.melo@gmail.com>
2014-07-24 01:35:35 -0300

    generating the first level of subclasses

--- gen.py
+++ gen.py
@@ -5,7 +5,17 @@ import xmltodict
 with open ("ontology.xml", "r") as myfile:
     data=myfile.read()
 
-xmlParsed = xmltodict.parse(data)
-for beerClasses in xmlParsed['beer']:
-	print "<owl:Class rdf:ID=\"" + beerClasses + "\" />"
+#parsing the XML file
+xml = xmltodict.parse(data)
+root = 'beer'
+
+#generating the classes
+for beerClass in xml[root]:
+	print "<owl:Class rdf:ID=\"" + beerClass + "\" />"
+	
+	#generating the subclasses
+	for subClass1 in xml[root][str(beerClass)]:
+		print "\t<owl:Class rdf:ID=\"" + subClass1 + "\">"
+		print "\t<rdfs:subClassOf rdf:resource=\"#" + beerClass + "\" />"
+	print "</owl:Class>"
 
