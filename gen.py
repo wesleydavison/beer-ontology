14e889dc39f3158522742863e73014a498597730
Wesley Davison <davison.melo@gmail.com>
2014-07-24 23:27:19 -0300

    Add header and subclass level 1

--- gen.py
+++ gen.py
@@ -1,5 +1,9 @@
 #!/usr/env/python
+'''
+Rule Generator to Expert SINTA engine
 
+by: Wesley Melo and Thiago Lima
+'''
 import xmltodict
 
 #opening input and output files
@@ -7,13 +11,20 @@ with open ("ontology.xml", "r") as myfile:
     data=myfile.read()
 outFile=open('./ontology.owl', 'w+')
 
-
 #parsing the XML file
 xml = xmltodict.parse(data)
 root = 'beer'
-output = ""
+output = "<?xml version=\"1.0\"?>\n \
+			<rdf:RDF \n \
+			 xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n \
+			 xmlns:xsd=\"http://www.w3.org/2001/XMLSchema#\"\n \
+   			 xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n \
+   			 xmlns:owl=\"http://www.w3.org/2002/07/owl#\"\n \
+    		xmlns:vcard=\"http://www.w3.org/2001/vcard-rdf/3.0#\">\n "
 
-#generating the classes
+#append header
+
+#generating the rules
 for beerClass in xml[root]:
 
 	output += "<owl:Class rdf:ID=\"" + beerClass.encode('utf-8') + "\">" + '\n'
@@ -22,20 +33,25 @@ for beerClass in xml[root]:
 	#generating the first level of subclass
 	for subClass1 in xml[root][str(beerClass)]:
 		output += "\t<owl:Class rdf:ID=\"" + subClass1.encode('utf-8') + "\">" + '\n'
-		output += "\t\t<rdfs:label xml:lang=\"en\">" + subClass1.encode('utf-8') + "</rdfs:label>" + '\n'
-		output += "\t\t<rdfs:subClassOf rdf:resource=\"#" + beerClass.encode('utf-8') + "\" />" + '\n'
+		#output += "\t\t<rdfs:label xml:lang=\"en\">" + subClass1.encode('utf-8') + "</rdfs:label>" + '\n'
+		output += "\t\t<rdfs:subClassOf>\n" 
+		output += "\t\t\t <owl:Class rdf:about=\"#" + beerClass.encode('utf-8') + "\"/>" + '\n'
+		output += "\t\t</rdfs:subClassOf>\n" 
 		output += "\t</owl:Class>" + '\n'
 
 		#generating second level of subclass
 		for subClass2 in xml[root][str(beerClass)][subClass1]:
 			output += "\t<owl:Class rdf:ID=\"" + subClass2.encode('utf-8') + "\">" + '\n'
-			output += "\t\t<rdfs:label xml:lang=\"en\">" + subClass2.encode('utf-8') + "</rdfs:label>" + '\n'
-			output += "\t\t<rdfs:subClassOf rdf:resource=\"#" + subClass1.encode('utf-8') + "\" />" + '\n'
+			#output += "\t\t<rdfs:label xml:lang=\"en\">" + subClass2.encode('utf-8') + "</rdfs:label>" + '\n'
+			#output += "\t\t<rdfs:subClassOf rdf:resource=\"#" + subClass1.encode('utf-8') + "\" />" + '\n'
 			output += "\t</owl:Class>" + '\n'
 
 	
 	#closing class
 	output += "</owl:Class>" + '\n'
+
+#closing rdf root tag
+output +=  "</rdf:RDF>"
 
 #writing in file
 outFile.write(output)
