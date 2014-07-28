07860b6933eebb171a5c2f58d8ac8eb7ebb05fd4
Wesley Davison <davison.melo@gmail.com>
2014-07-28 19:17:16 -0300

    Indent Sinta output file to Sinta style

--- gen.py
+++ gen.py
@@ -61,26 +61,26 @@ for beerClass in xml[root]:
 			   or subClass2 == 'Mouth-feel':
 				for subClass3 in xml[root][str(beerClass)][subClass1][subClass2]:
 					
-					conditionClass2 = "SE " if firstTimeClass2 else "E "
-					outSintaFile += ident(1) + conditionClass2 + \
+					conditionClass2 = "SE" if firstTimeClass2 else "E"
+					outSintaFile += conditionClass2 + "\n" + ident(1) +\
 									subClass3.encode('utf-8') + " = " + \
 									xml[root][str(beerClass)][subClass1][subClass2][subClass3] + '\n'
 					firstTimeClass2 = False	
 			
 			if subClass2 == 'Samples':
 				#no more conditions. Time to close Sinta rule 
-				outSintaFile += ident(1) + u"ENTÃO sub-classe de cerveja = " + subClass1.encode('utf-8') + " CNF 100%" +'\n\n'
+				outSintaFile += u"ENTÃO\n" + ident(1) + "sub-classe de cerveja = " + subClass1.encode('utf-8') + " CNF 100%" +'\n\n'
 
 				for subClass3 in xml[root][str(beerClass)][subClass1][subClass2]:
 					outSintaFile += "Regra " + subClass3 + "\n"
-					outSintaFile += ident(1) + "SE sub-classe de cerveja = " + subClass1.encode('utf-8') +  "\n"
+					outSintaFile += "SE\n" + ident(1) + "sub-classe de cerveja = " + subClass1.encode('utf-8') +  "\n"
 					firstTimeClass3 = True
 					for subClass4 in xml[root][str(beerClass)][subClass1][subClass2][subClass3]:
-						outSintaFile += ident(1) + "E "+ \
+						outSintaFile += "E\n"+ ident(1) + \
 											subClass4.encode('utf-8') + " = " + \
 											xml[root][str(beerClass)][subClass1][subClass2][subClass3][subClass4] + '\n'
 						firstTimeClass3 = False		
-					outSintaFile += ident(1) + u"ENTÃO nome de cerveja = " + subClass3 + " CNF 100%" + '\n\n'		
+					outSintaFile +=  u"ENTÃO \n" + ident(1) + "nome de cerveja = " + subClass3 + " CNF 100%" + '\n\n'		
 
 					outOntFile += ident(1) + "<owl:NamedIndividual rdf:about=\"#" + subClass3.encode('utf-8') + "\">" + '\n'
 					outOntFile += ident(2) + "<rdf:type rdf:resource=\"#" + subClass1.encode('utf-8') + "\"/>" + '\n'
